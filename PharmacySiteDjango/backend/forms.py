7df7d3927d7a868eb91a03f20ccb19dfa84a28aa
Victor Polyakov <polyakov.victor21@gmail.com>
2023-12-01 22:55:42 +0200

    edit form

--- PharmacySiteDjango/backend/forms.py
+++ PharmacySiteDjango/backend/forms.py
@@ -5,15 +5,20 @@ from .models import Order
 
 class OrderForm(forms.ModelForm):
     medicine_id = forms.IntegerField(widget=forms.HiddenInput())
-    quantity = forms.IntegerField(min_value=1, initial=1, widget=forms.NumberInput(attrs={'class': 'form-control', 'style': 'width: 50px;'}))
+    quantity = forms.IntegerField(min_value=1, initial=1, label='Кількість', widget=forms.NumberInput(attrs={'class': 'form-control', 'style': 'width: 50px;'}))
 
     class Meta:
         model = Order
         fields = ['pharmacy', 'full_name', 'phone_number', 'quantity']
+        labels = {
+            'pharmacy': 'Замовити в аптеку на вулиці:',
+            'full_name': 'Повне ім`я:',
+            'phone_number': 'Номер телефону:',
+        }
         widgets = {
-            'pharmacy': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Выберите аптеку'}),
-            'full_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите имя и фамилию'}),
-            'phone_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите номер телефона'}),
+            'pharmacy': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Виберіть аптеку'}),
+            'full_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введіть ім`я та прізвище'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введіть номер телефону'}),
         }
 
     def __init__(self, *args, **kwargs):
